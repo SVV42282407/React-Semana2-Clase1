4886e87acbc24acf4749aa898ad194b8e545ce77
Josue Rodriguez <josue.rodriguez@ASIGBIEM.ORG>
2021-10-01 22:41:27 -0500

    Retos

--- reto/src/data/cards.js
+++ reto/src/data/cards.js
@@ -2,44 +2,32 @@ export const cards = [
     {
         name: 'Lorem ipsum',
         productDescription: 'Lorem ipsum dolor sit amet consectetur adipiscing elit id parturient erat',
-        url: 'https://www.ipp.edu.pe/blog/wp-content/uploads/2020/06/shutterstock_573386038.jpg',
-        size: 140,
-        diet: ['meal']
+        url: 'https://www.ipp.edu.pe/blog/wp-content/uploads/2020/06/shutterstock_573386038.jpg'
     },
     {
         name: 'amet consectetur adipiscing',
         productDescription: 'Lorem ipsum dolor sit amet consectetur adipiscing elit id parturient erat',
-        url: 'https://www.ipp.edu.pe/blog/wp-content/uploads/2020/06/shutterstock_573386038.jpg',
-        size: 140,
-        diet: ['meal']
+        url: 'https://www.ipp.edu.pe/blog/wp-content/uploads/2020/06/shutterstock_573386038.jpg'
     },
     {
         name: 'luctus class tincidunt',
         productDescription: 'Lorem ipsum dolor sit amet consectetur adipiscing elit id parturient erat',
-        url: 'https://www.ipp.edu.pe/blog/wp-content/uploads/2020/06/shutterstock_573386038.jpg',
-        size: 140,
-        diet: ['meal']
+        url: 'https://www.ipp.edu.pe/blog/wp-content/uploads/2020/06/shutterstock_573386038.jpg'
     },
     {
         name: 'Nisi vestibulum',
         productDescription: 'Lorem ipsum dolor sit amet consectetur adipiscing elit id parturient erat',
-        url: 'https://www.ipp.edu.pe/blog/wp-content/uploads/2020/06/shutterstock_573386038.jpg',
-        size: 140,
-        diet: ['meal']
+        url: 'https://www.ipp.edu.pe/blog/wp-content/uploads/2020/06/shutterstock_573386038.jpg'
     },
     {
         name: 'curae imperdiet',
         productDescription: 'Lorem ipsum dolor sit amet consectetur adipiscing elit id parturient erat',
-        url: 'https://www.ipp.edu.pe/blog/wp-content/uploads/2020/06/shutterstock_573386038.jpg',
-        size: 120,
-        diet: ['meal']
+        url: 'https://www.ipp.edu.pe/blog/wp-content/uploads/2020/06/shutterstock_573386038.jpg'
     },
     {
         name: 'ligula volutpat',
         productDescription: 'Lorem ipsum dolor sit amet consectetur adipiscing elit id parturient erat',
-        url: 'https://www.ipp.edu.pe/blog/wp-content/uploads/2020/06/shutterstock_573386038.jpg',
-        size: 200,
-        diet: ['plant', 'insects'],
+        url: 'https://www.ipp.edu.pe/blog/wp-content/uploads/2020/06/shutterstock_573386038.jpg'
         additional: {
             notes: 'This is th eastern gorilla. There is also a western gorilla.'
         }
